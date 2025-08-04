f597082c4375cee9222088c49708e52142b99e0f
Atharva Patil <anpatil.1223@gmail.com>
2025-08-04 21:41:10 +0530

    refactor intDivision to handle division by zero; update main to display error messages and format output

--- cmd/functions_golang/main.go
+++ cmd/functions_golang/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	var printValue string = "Hello world" //since parameter type is enforced so you can't put other data type
@@ -8,16 +11,28 @@ func main() {
 
 	var numerator int = 12
 	var denominator int = 4
-	var result, remainder int = intDivision(numerator, denominator)
-	fmt.Printf("The result of the integer division is %v and the remainder of the integer division is %v ", result, remainder)
+	var result, remainder, err = intDivision(numerator, denominator)
+	if err != nil {
+		fmt.Printf(err.Error())
+	} else if remainder == 0 {
+		fmt.Printf("the result of the integer division is %v", result)
+	} else {
+		fmt.Printf("The result of the integer division is %v and the remainder of the integer division is %v ", result, remainder)
+	}
 }
 
 func printMe(printValue string) { //the opening braces should be here only otherweise go throws error
 	fmt.Println(printValue)
 }
 
-func intDivision(numerator int, denominator int) (int, int) { // the int delclared here suggest a int value is gonna be returned
+func intDivision(numerator int, denominator int) (int, int, error) { // the int delclared here suggest a int value is gonna be returned
+
+	var err error
+	if denominator == 0 {
+		err = errors.New("cannot divide By zero")
+		return 0, 0, err
+	}
 	var result int = numerator / denominator
 	var remainder int = numerator % denominator
-	return result, remainder
+	return result, remainder, err
 }
