a76f0c4360a7e47d172a040f7ebe355f22200bf4
Atharva Patil <anpatil.1223@gmail.com>
2025-08-06 23:10:06 +0530

    add examples for different array declarations and introduce a slice in main function

--- cmd/array,slice,maps/main.go
+++ cmd/array,slice,maps/main.go
@@ -3,16 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var intArr [3]int32 //the array holds exatcly 3 elements and cannot change after initialised
-
+	//different ways to declare an array
+	var intArr [3]int32                     //the array holds exatcly 3 elements and cannot change after initialised
 	var Array2 [3]int32 = [3]int32{1, 2, 3} //imidiatly initialise the array
-
 	//or
-
 	Array3 := [3]int32{1, 2, 3}
-
 	//or
-
 	Array4 := [...]int32{1, 2, 3, 4}
 
 	fmt.Println(Array3)
@@ -21,5 +17,8 @@ func main() {
 	intArr[1] = 123
 	fmt.Println(intArr[0])
 	fmt.Println(intArr[1:3])
+
+	var intSlice []int32 = []int32{4, 5, 6}
+	fmt.Println(intSlice)
 
 }
